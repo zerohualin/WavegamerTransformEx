a8e59925d8b1272a22a0d456fab9dfd138e9a58b
hualinzero <286990256@qq.com>
2021-02-09 14:49:31 +0800

    -

---
+++ Assets/Runtime/TransformExx.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wavegamer.TransformEx
+{
+	public static class TransformExx
+	{
+		public static string TranslocationToString(this Transform tra)
+		{
+			return tra.localPosition.ToString();
+		}
+	}
+}
